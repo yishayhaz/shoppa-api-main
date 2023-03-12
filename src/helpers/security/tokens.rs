6c7f95eb091b199e83a92f7d0a843a32eaa71171
omer cohen <omerco@saitechs.io>
2023-03-12 23:33:53 +0200

    generate login token

--- src/helpers/security/tokens.rs
+++ src/helpers/security/tokens.rs
@@ -3,49 +3,46 @@ use crate::helpers::env::EnvVars;
 use crate::helpers::types::ResponseBuilder;
 use axum::response::{IntoResponse, Response};
 use chrono::Utc;
-use jwt_simple::prelude::*;
+use rusty_paseto::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::error::Error;
 
 lazy_static! {
-    static ref JWT_LOGIN_KEY: HS256Key =
-        HS256Key::from_bytes(&EnvVars::JWT_LOGIN_SECRET.get().as_bytes());
+    static ref JWT_LOGIN_KEY: PasetoSymmetricKey<V4, Local> =
+        PasetoSymmetricKey::from(Key::from(EnvVars::JWT_LOGIN_SECRET.get().as_bytes()));
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LoginTokenData {
     pub id: String,
     pub level: i32,
-    pub created_at: i64,
 }
 
 pub fn generate_login_token(user: &User) -> Result<String, Response> {
-    let data = LoginTokenData {
+    let token_data = LoginTokenData {
         id: user.id()?.to_string(),
         level: user.level,
-        created_at: Utc::now().timestamp(),
     };
 
-    let claims = Claims::with_custom_claims(data, Duration::from_days(90));
+    let token_builder = || -> Result<String, Box<dyn Error>> {
+        let in_90_days = (Utc::now() + chrono::Duration::days(90)).to_rfc3339();
 
-    match JWT_LOGIN_KEY.authenticate(claims) {
+        let token = PasetoBuilder::<V4, Local>::default()
+            .set_claim(ExpirationClaim::try_from(in_90_days)?)
+            .set_claim(IssuerClaim::try_from("main-api")?)
+            .set_claim(CustomClaim::try_from(("payload", token_data))?)
+            .build(&JWT_LOGIN_KEY)?;
+
+        Ok(token)
+    };
+
+    match token_builder() {
         Ok(token) => Ok(token),
         Err(_) => Err(ResponseBuilder::<u16>::error(
             None,
-            Some(String::from("Faild to create login token")),
-            Some(500),
-        )
-        .into_response()),
-    }
-}
-
-pub fn verify_login_token(token: &str) -> Result<JWTClaims<LoginTokenData>, Response> {
-    // TODO improve this
-    match JWT_LOGIN_KEY.verify_token::<LoginTokenData>(&token, None){
-        Ok(claims) => Ok(claims),
-        Err(_) => Err(ResponseBuilder::<u16>::error(
-            None,
-            Some(String::from("Faild to verify login token")),
+            Some(String::from(
+                "Internal Server Error while generating login token",
+            )),
             Some(500),
         )
         .into_response()),
