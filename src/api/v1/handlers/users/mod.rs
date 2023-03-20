53bc87027248052d5f3b2f265016b9c9402f497f
omer cohen <omerco@saitechs.io>
2023-03-21 00:27:29 +0200

    route name change

--- src/api/v1/handlers/users/mod.rs
+++ src/api/v1/handlers/users/mod.rs
@@ -8,6 +8,6 @@ pub fn router() -> Router {
     Router::new()
         .route("/login", routing::post(login::login))
         .route("/logout", routing::delete(login::logout))
-        .route("/sign-up/", routing::post(signup::signup))
+        .route("/sign-up", routing::post(signup::signup))
         .route("/sign-up/1", routing::post(signup::signup_level_1))
 }
