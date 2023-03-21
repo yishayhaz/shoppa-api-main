a1442e6abb85befbbc7c6a0b3944776f725e5971
omer cohen <omerco@saitechs.io>
2023-03-21 23:21:37 +0200

    allowed headers fix

--- src/helpers/security/cors.rs
+++ src/helpers/security/cors.rs
@@ -1,25 +1,113 @@
 use crate::helpers::env::EnvVars;
-use http::{request::Parts as RequestParts, HeaderValue};
-use tower_http::cors::{CorsLayer, AllowOrigin};
+use http::{header, request::Parts as RequestParts, HeaderName, HeaderValue, Method};
+use tower_http::cors::{AllowHeaders, AllowOrigin, CorsLayer};
 
 pub fn get_cors_layer() -> CorsLayer {
     let methods = [
-        "POST".parse().unwrap(),
-        "GET".parse().unwrap(),
-        "PUT".parse().unwrap(),
-        "PATCH".parse().unwrap(),
-        "DELETE".parse().unwrap(),
-        "OPTIONS".parse().unwrap(),
+        Method::POST,
+        Method::GET,
+        Method::PUT,
+        Method::PATCH,
+        Method::DELETE,
+        Method::OPTIONS,
+        Method::HEAD,
     ];
 
-    let cors = CorsLayer::new()
+    let headers = [
+        header::CONTENT_TYPE,
+        header::ACCEPT,
+        header::ACCEPT_CHARSET,
+        header::ACCEPT_ENCODING,
+        header::ACCEPT_LANGUAGE,
+        header::ACCEPT_RANGES,
+        header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
+        header::ACCESS_CONTROL_ALLOW_HEADERS,
+        header::ACCESS_CONTROL_ALLOW_METHODS,
+        header::ACCESS_CONTROL_ALLOW_ORIGIN,
+        header::ACCESS_CONTROL_EXPOSE_HEADERS,
+        header::ACCESS_CONTROL_MAX_AGE,
+        header::ACCESS_CONTROL_REQUEST_HEADERS,
+        header::ACCESS_CONTROL_REQUEST_METHOD,
+        header::AGE,
+        header::ALLOW,
+        header::ALT_SVC,
+        header::AUTHORIZATION,
+        header::CACHE_CONTROL,
+        header::CACHE_STATUS,
+        header::CDN_CACHE_CONTROL,
+        header::CONNECTION,
+        header::CONTENT_DISPOSITION,
+        header::CONTENT_ENCODING,
+        header::CONTENT_LANGUAGE,
+        header::CONTENT_LENGTH,
+        header::CONTENT_LOCATION,
+        header::CONTENT_RANGE,
+        header::CONTENT_SECURITY_POLICY,
+        header::CONTENT_SECURITY_POLICY_REPORT_ONLY,
+        header::CONTENT_TYPE,
+        header::COOKIE,
+        header::DNT,
+        header::DATE,
+        header::ETAG,
+        header::EXPECT,
+        header::EXPIRES,
+        header::FORWARDED,
+        header::FROM,
+        header::HOST,
+        header::IF_MATCH,
+        header::IF_MODIFIED_SINCE,
+        header::IF_NONE_MATCH,
+        header::IF_RANGE,
+        header::IF_UNMODIFIED_SINCE,
+        header::LAST_MODIFIED,
+        header::LINK,
+        header::LOCATION,
+        header::MAX_FORWARDS,
+        header::ORIGIN,
+        header::PRAGMA,
+        header::PROXY_AUTHENTICATE,
+        header::PROXY_AUTHORIZATION,
+        header::PUBLIC_KEY_PINS,
+        header::PUBLIC_KEY_PINS_REPORT_ONLY,
+        header::RANGE,
+        header::REFERER,
+        header::REFERRER_POLICY,
+        header::REFRESH,
+        header::RETRY_AFTER,
+        header::SEC_WEBSOCKET_ACCEPT,
+        header::SEC_WEBSOCKET_EXTENSIONS,
+        header::SEC_WEBSOCKET_KEY,
+        header::SEC_WEBSOCKET_PROTOCOL,
+        header::SEC_WEBSOCKET_VERSION,
+        header::SERVER,
+        header::SET_COOKIE,
+        header::STRICT_TRANSPORT_SECURITY,
+        header::TE,
+        header::TRAILER,
+        header::TRANSFER_ENCODING,
+        header::UPGRADE,
+        header::UPGRADE_INSECURE_REQUESTS,
+        header::USER_AGENT,
+        header::VARY,
+        header::VIA,
+        header::WARNING,
+        header::WWW_AUTHENTICATE,
+        header::X_CONTENT_TYPE_OPTIONS,
+        header::X_DNS_PREFETCH_CONTROL,
+        header::X_FRAME_OPTIONS,
+        header::X_XSS_PROTECTION,
+        header::HeaderName::from_static("x-real-ip"),
+    ];
+
+    CorsLayer::new()
         .allow_methods(methods)
         .allow_credentials(true)
         .allow_origin(AllowOrigin::predicate(
-        |origin: &HeaderValue, _request_parts: &RequestParts| {
-            origin.as_bytes().ends_with(EnvVars::COOKIE_DOMAIN.get().as_bytes())
-        },
-    ));
-
-    cors
+            |origin: &HeaderValue, _request_parts: &RequestParts| {
+                origin
+                    .as_bytes()
+                    .ends_with(EnvVars::COOKIE_DOMAIN.get().as_bytes())
+            },
+        ))
+        .allow_headers(AllowHeaders::list(headers))
 }
