f803aab341baf06d262ab603f5fd323a5f89dc8d
omer cohen <omerco@saitechs.io>
2023-03-21 19:29:04 +0200

    added domain schema for cors

--- src/helpers/security/cors.rs
+++ src/helpers/security/cors.rs
@@ -1,15 +1,23 @@
-use tower_http::cors::CorsLayer;
 use crate::helpers::env::EnvVars;
+use tower_http::cors::CorsLayer;
 
 pub fn get_cors_layer() -> CorsLayer {
-    let origins = [EnvVars::COOKIE_DOMAIN.get().parse().unwrap()];
+    let mut schema = "http://";
+
+    if EnvVars::is_production() {
+        schema = "https://";
+    };
+
+    let origins = [format!("{}{}", schema, EnvVars::COOKIE_DOMAIN.get())
+        .parse()
+        .unwrap()];
 
     let methods = [
         "POST".parse().unwrap(),
         "GET".parse().unwrap(),
         "PUT".parse().unwrap(),
         "PATCH".parse().unwrap(),
-        "DELETE".parse().unwrap()
+        "DELETE".parse().unwrap(),
     ];
 
     CorsLayer::new()
