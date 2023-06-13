d34b91fa89c0e7b9d65468abb87b4fca6fd9a468
Yishay Hazan <yishay11013@gmail.com>
2023-06-13 19:17:55 +0300

    typo

--- src/api/management/handlers/products/mod.rs
+++ src/api/management/handlers/products/mod.rs
@@ -8,7 +8,7 @@ pub fn router() -> Router {
     Router::new()
         .nest("/:product_id/items", items::router())
         .route(
-            "/:product_id/upload-files",
+            "/:product_id/upload-file",
             routing::put(routes::upload_product_images),
         )
         .route("/:product_id", routing::patch(routes::edit_product))
