3db878501609f87b2cb4c0533d3985018083d494
omer cohen <omerco@saitechs.io>
2023-07-10 18:30:58 +0300

    you can layer a route

--- src/api/v1/handlers/users/cart/mod.rs
+++ src/api/v1/handlers/users/cart/mod.rs
@@ -1,5 +1,5 @@
 use crate::api::v1::middlewares;
-use axum::{routing, Router, middleware};
+use axum::{middleware, routing, Router};
 mod routes;
 mod types;
 
@@ -9,11 +9,11 @@ pub fn router() -> Router {
         .route("/", routing::delete(routes::remove_product_from_cart))
         .route("/", routing::patch(routes::edit_product_in_cart))
         .route_layer(middleware::from_fn(middlewares::login_required))
-        .nest("/add", add_to_cart_router())
+        .route(
+            "/",
+            routing::post(routes::add_product_to_cart).layer(middleware::from_fn(
+                middlewares::login_required_or_create_guest,
+            )),
+        )
 }
 
-fn add_to_cart_router() -> Router {
-    Router::new()
-        .route("/", routing::post(routes::add_product_to_cart))
-        .route_layer(middleware::from_fn(middlewares::login_required_or_create_guest))
-}
