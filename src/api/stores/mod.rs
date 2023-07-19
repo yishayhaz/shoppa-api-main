30f758b788c1956e12379a46667094ff77c67b46
Yishay Hazan <yishay11013@gmail.com>
2023-07-19 07:27:30 +0300

    added logout

--- src/api/stores/mod.rs
+++ src/api/stores/mod.rs
@@ -5,6 +5,7 @@ use axum::{middleware, Router};
 pub fn router() -> Router {
     Router::new()
         .nest("/me", handlers::me::router())
+        .nest("/logout", handlers::logout::router())
         .nest("/products", handlers::products::router())
         .nest("/variants", handlers::variants::router())
         .nest("/store", handlers::store::router())
