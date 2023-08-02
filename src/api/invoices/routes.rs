1c35fbeb4a669953b9834801b484a3be51c5c953
Yishay Hazan <yishay11013@gmail.com>
2023-08-02 08:21:24 +0300

    update mail sent

--- src/api/invoices/routes.rs
+++ src/api/invoices/routes.rs
@@ -2,12 +2,15 @@ use crate::{db::AxumDBExtansion, prelude::*};
 use axum::{extract::Path, response::IntoResponse};
 use bson::{doc, oid::ObjectId};
 use shoppa_core::ResponseBuilder;
+use std::env;
 
 pub async fn update_mail_sent(
     db: AxumDBExtansion,
     Path((token, order_id)): Path<(String, ObjectId)>,
 ) -> HandlerResult {
-    if token.is_empty() || token != "token" {
+    let invoice_service_secret = env::var("INVOICE_SERVICE_SECRET").unwrap_or_default();
+
+    if token.is_empty() || token != invoice_service_secret {
         return Ok(
             ResponseBuilder::<u16>::error("", None, Some("hello stranger 👋"), Some(404))
                 .into_response(),
@@ -15,7 +18,7 @@ pub async fn update_mail_sent(
     }
 
     let filter = doc! {"order_id": order_id };
-    let update = doc! {"$set": {"mail_sent": true}};
+    let update = doc! {"$set": { "mail_sent": true }};
 
     let _ = db.update_many_invoices(filter, update, None, None).await?;
 
